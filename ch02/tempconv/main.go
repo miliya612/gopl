5b6957e62288b637a3842b733e1c2d1f80c5c4cd
miliya12 <t.miliya612@gmail.com>
2018-09-17 17:44:09 +0900

    add ch02

---
+++ ch02/tempconv/main.go
@@ -0,0 +1,24 @@
+package tempconv
+
+import (
+	"fmt"
+	"gopl.io/ch2/tempconv"
+)
+
+type Celsius float64
+type Fahrenheit float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC Celsius = 0
+	BoilingC Celsius = 100
+)
+
+func main() {
+	fmt.Println(tempconv.CToF(tempconv.AbsoluteZeroC))
+}
+
+func (c Celsius) String() string {return fmt.Sprintf("%g˚C", c)}
+
+func (f Fahrenheit) String() string {return fmt.Sprintf("%g˚F", f)}
+
